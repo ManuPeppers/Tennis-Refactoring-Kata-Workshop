4a145c95a38dc2753a4f2fc042ad1ef1d59aa8e9
ManuPeppers <manuel.grimaldi@xpeppers.com>
2019-06-11 17:05:26 +0200

    added test class

--- java/src/main/java/TennisGame.java
+++ java/src/main/java/TennisGame.java
@@ -46,11 +46,14 @@ public class TennisGame {
             int minusResult = m_score1-m_score2;
             if (minusResult==1) {
                 score ="Advantage player1";
-            } else if (minusResult ==-1) {
+            }
+            else if (minusResult ==-1) {
                 score ="Advantage player2";
-            } else if (minusResult>=2) {
+            }
+            else if (minusResult>=2) {
                 score = "Win for player1";
-            } else {
+            }
+            else {
                 score ="Win for player2";
             }
         }
@@ -60,7 +63,9 @@ public class TennisGame {
             {
                 if (i==1) {
                     tempScore = m_score1;
-                } else { score+="-"; tempScore = m_score2;}
+                }
+                else { score+="-"; tempScore = m_score2;
+                }
                 switch(tempScore)
                 {
                     case 0:
---
+++ java/src/test/java/TennisGameTest.java
@@ -0,0 +1,209 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TennisGameTest {
+
+    @Test
+    public void getScoreStartGame() {
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        assertEquals("Love-All",tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreFifteenAll(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player2");
+
+        assertEquals("Fifteen-All", tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreFifteeThirtyAll(){
+
+        TennisGame tennisGame = new TennisGame("P1", "P2");
+
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player2");
+
+        assertEquals("Thirty-All", tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreDeuce(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player2");
+
+        assertEquals("Deuce",tennisGame.getScore());
+    }
+
+
+    @Test
+    public void getScoreFifteenLove(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player1");
+
+        assertEquals("Fifteen-Love",tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreThirtyLove(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+
+        assertEquals("Thirty-Love",tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreFortyLove(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+
+        assertEquals("Forty-Love",tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreLoveFifteen(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player2");
+
+        assertEquals("Love-Fifteen",tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreLoveThirty(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+
+        assertEquals("Love-Thirty",tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreLoveForty(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+
+        assertEquals("Love-Forty",tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreAdvantagePlayer1(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+
+        assertEquals("Advantage player1", tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreAdvantagePlayer2(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+
+        assertEquals("Advantage player2", tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreWinForPlayer2(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+
+        assertEquals("Win for player2", tennisGame.getScore());
+
+    }
+
+    @Test
+    public void getScoreWinForPlayer1(){
+
+        TennisGame tennisGame = new TennisGame("P1","P2");
+
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player1");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+        tennisGame.wonPoint("player2");
+
+        assertEquals("Win for player1", tennisGame.getScore());
+
+    }
+
+
+}
